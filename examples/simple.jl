2c5fbb1b0f6969b5436de9c2fd9726159a39745b
McCoy Becker <mccoybecker@gmail.com>
2021-05-12 02:57:43 -0400

    Updated README with a bit more info on this experiment.

--- examples/simple.jl
+++ examples/simple.jl
@@ -1,10 +1,9 @@
 module Simple
 
 using Purple
-using Mixtape
 using SymbolicUtils: Sym 
-using SymbolicUtils.Code
 
+# Works with method representations of control flow.
 b(x) = x + 10
 function fn(x, y)
     ifelse(x > 5, b(getfield((x + y, ), 1)), 10)
@@ -26,6 +25,11 @@ f = lift(fn,
          Tuple{Int, Int};
          jit = true,
          opt = true)
+
+# This is an annoying thing which should be hidden.
+# Because we use a version of "sneaky invoke" but can't modify the signature
+# we have to pass in a set of "blank" args of the original type (here, (0, 0))
+# because our semantic stub function _lift has signature _lift(fn, syms::Tuple, args::Tuple) to piggyback off type inference for signature Tuple{typeof(fn), map(typeof, args)...}
 ret = f(fn, (Sym{Int}(:x), Sym{Int}(:y)), (0, 0))
 display(ret)
 
