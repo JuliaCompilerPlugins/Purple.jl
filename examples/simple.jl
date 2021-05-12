730c7269c3a9b5bf9d37697f9e56e1a9bfb0dd3c
McCoy Becker <mccoybecker@gmail.com>
2021-05-12 18:20:30 -0400

    Removed requirement of 'blank' arguments.

--- examples/simple.jl
+++ examples/simple.jl
@@ -5,20 +5,28 @@ using SymbolicUtils: Sym
 
 b(x::Int) = 10 + x
 function fn(x::Int, y::Int)
-    z = x + y
+    z = 0
+    z += x + y
     return b(z) + 10
 end
 
 src = lift(fn,
            Tuple{Int, Int};
            jit = false,
-           opt = false)
+           opt = true)
 display(src)
 
 f = lift(fn,
          Tuple{Int, Int};
          jit = true,
          opt = false)
+ret = f(Sym{Int}(:x), Sym{Int}(:y))
+display(ret)
+
+f = lift(fn,
+         Tuple{Int, Int};
+         jit = true,
+         opt = true)
 ret = f(Sym{Int}(:x), Sym{Int}(:y))
 display(ret)
 
--- src/Purple.jl
+++ src/Purple.jl
@@ -19,56 +19,70 @@ end
 struct LiftContext <: CompilationContext
     opt::Bool
 end
+
 allow(ctx::LiftContext, mod::Module, fn::typeof(_lift), inner, args...) = true
 allow(ctx::LiftContext, mod::Module, fn::typeof(_lift), inner::typeof(SymbolicUtils.add_t), args...) = false
 allow(ctx::LiftContext, mod::Module, fn::typeof(_lift), inner::typeof(SymbolicUtils.makeadd), args...) = false
 
+# Wrap each call with `_lift`.
+# Otherwise, if the Expr has .head == :(=), try and wrap the RHS.
+function swap!(b, v, st::Expr, slotmap)
+    if st.head == :call
+        args = walk(l -> resolve(get(slotmap, l, l)), st).args[2 : end]
+        atup = push!(b, Expr(:call, tuple, args...))
+        return Expr(:call, _lift, st.args[1], atup)
+    elseif st.head == :(=) && 
+        st.args[2] isa Expr &&
+        st.args[2].head == :call
+        callexpr = st.args[2]
+        args = walk(l -> resolve(get(slotmap, l, l)), callexpr).args[2 : end]
+        atup = push!(b, Expr(:call, tuple, args...))
+        return Expr(:(=), st.args[1], 
+                    Expr(:call, _lift, callexpr.args[1], atup))
+    else
+        return st
+    end
+end
+
+swap!(b, v, st, slotmap) = walk(l -> get(slotmap, l, l), st)
+
 function transform(mix::LiftContext, src, sig)
+    
     if !(sig[2] <: Function) || 
         sig[2] === Core.IntrinsicFunction
         return src
     end # If target is not a function, just return src.
 
+    # Create Builder and try to get the src from the inner
+    # call using the unwrapped Sym arg types.
     b = CodeInfoTools.Builder(src)
-    forward = sig[2].instance
+    inner = sig[2].instance
     symtypes = sig[3]
     argtypes = Tuple{map(unwrap, sig[3].parameters)...}
     largtypes = length(argtypes.parameters)
     try
-        forward = CodeInfoTools.code_info(forward, argtypes)
+        inner = CodeInfoTools.code_info(inner, argtypes)
     catch
         return src
     end
-    forward === nothing && return src
-    slotmap = Dict()
+    inner === nothing && return src
 
     # Slots as args.
-    for (ind, a) in enumerate(forward.slotnames[2 : 1 + largtypes])
+    slotmap = Dict()
+    for (ind, a) in enumerate(inner.slotnames[2 : 1 + largtypes])
         o = push!(b, Expr(:call, Base.getindex, slot(3), ind))
-        setindex!(slotmap, o, get_slot(forward, a))
+        setindex!(slotmap, o, get_slot(inner, a))
     end
 
     # Extra slots.
-    for (ind, a) in enumerate(forward.slotnames[2 + largtypes : end])
+    for (ind, a) in enumerate(inner.slotnames[2 + largtypes : end])
         s = slot!(b, a)
     end
 
-    for (v, st) in enumerate(forward.code)
-        if st isa Expr
-            if st.head == :call
-                args = walk(v -> resolve(get(slotmap, v, v)), st).args[2 : end]
-                atup = push!(b, Expr(:call, tuple, args...))
-                ex = Expr(:call, _lift, st.args[1], atup)
-            elseif st.head == :(=)
-                callexpr = st.args[2]
-                args = walk(v -> resolve(get(slotmap, v, v)), callexpr).args[2 : end]
-                atup = push!(b, Expr(:call, tuple, args...))
-                ex = Expr(:(=), st.args[1], 
-                          Expr(:call, _lift, callexpr.args[1], atup))
-            end
-        else
-            ex = walk(v -> get(slotmap, v, v), st)
-        end
+    # Now "inline" the src code from inner into the builder.
+    # Make the right corrections to slots, etc.
+    for (v, st) in enumerate(inner.code)
+        ex = swap!(b, v, st, slotmap)
         setindex!(slotmap, push!(b, ex), var(v))
     end
     return CodeInfoTools.finish(b)
@@ -83,6 +97,9 @@ function optimize!(ctx::LiftContext, b)
         ir = getfield_elim_pass!(ir)
         ir = adce_pass!(ir)
         ir = compact!(ir)
+    end
+    if any(==(Union{}), ir.stmts.type)
+        @debug "Inference failed to infer -- found Union{}" ir
     end
     return ir
 end
